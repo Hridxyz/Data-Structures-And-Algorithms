d480408c54b4ed10e66a8fa176b3600ffc4330d5
Hriday Paras Bhagtani <96878347+Hridxyz@users.noreply.github.com>
2023-12-23 00:33:37 +0530

    Practicing

--- src/TwoDimensionalArrays/ArraySearch.java
+++ src/TwoDimensionalArrays/ArraySearch.java
@@ -25,6 +25,7 @@ public class ArraySearch {
                 {15,25,35,45},
                 {27,29,37,48},
                 {32,33,39,50}
+
         };
         int key = 33;
         System.out.print(staircaseSearch(matrix, key));
