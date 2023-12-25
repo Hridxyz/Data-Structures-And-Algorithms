af82120a2b1e689bfb25a38dbae0818746502d88
Hriday Paras Bhagtani <96878347+Hridxyz@users.noreply.github.com>
2023-12-25 18:29:57 +0530

    Practice

---
+++ src/Strings/stringscompare.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class stringscompare {
+    public static void main(String args []) {
+        String s1 = "tony";
+        String s2 = "tony";
+        String s3 = new String("tony");
+        if (s1 == s3) {
+            System.out.println ("Strings are equal");
+        }
+        else
+        {
+            System.out.println("Strings are not equal");
+        }
+        if (s1.equals(s3)) {
+            System.out.println("Strings are equal");
+
+        }
+        else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
